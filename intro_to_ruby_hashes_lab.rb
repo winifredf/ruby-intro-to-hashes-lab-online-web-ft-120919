95b198c30a5b6a8f7e3e701444cc5c15e1781d1c
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 05:09:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,6 +36,12 @@ def monopoly_with_second_tier
   monopoly[:railroads][:names][:b_and_o_railroad] = {}
   monopoly[:railroads][:names][:shortline] = {}
   
+  monopoly
+end
+
+
+
+  
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
