d04aba4a72bf8d1474db84ed48f8ecd25018bfda
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 05:11:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,7 +40,26 @@ def monopoly_with_second_tier
 end
 
 
-
+monopoly = {}
+  
+  monopoly[:railroads] = {}
+  
+  monopoly[:railroads][:pieces] = 4
+  monopoly[:railroads][:names] = {}
+  monopoly[:railroads][:rent_in_dollars] = {}
+  
+  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  
+  
+  monopoly[:railroads][:names][:reading_railroad] = {}
+  monopoly[:railroads][:names][:pennsylania_railroad] = {}
+  monopoly[:railroads][:names][:b_and_o_railroad] = {}
+  monopoly[:railroads][:names][:shortline] = {}
+  
+  monopoly
   
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
