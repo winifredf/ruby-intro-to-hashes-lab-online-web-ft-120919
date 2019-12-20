7eb58ad6790e3e33e5d569e40304d85f9d616fec
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 05:07:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,6 +32,7 @@ def monopoly_with_second_tier
   
   
   monopoly[:railroads][:names]
+  monopoly[:railroads][:names]
   
   
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
