cbfc592d3710cd6c1ace04fb32bcdcbbb83bb608
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 05:03:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_second_tier
   
   monopoly[:railroads][:pieces] = 4
   monopoly[:railroads][:names] = {}
-  monopoly[:railroads][:rent_in_dollars]{}
+  monopoly[:railroads][:rent_in_dollars] = {}
   
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
