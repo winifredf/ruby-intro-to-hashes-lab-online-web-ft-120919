7063974a79632ec9a5a3b6885816ca1bac01ec58
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 05:24:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -73,12 +73,12 @@ def monopoly_with_fourth_tier
   
   
   monopoly[:railroads][:names][:reading_railroad] = {}
-  monopoly[:railroads][:names][:pennsylania_railroad] = {}
+  monopoly[:railroads][:names][:pennsylvania_railroad] = {}
   monopoly[:railroads][:names][:b_and_o_railroad] = {}
   monopoly[:railroads][:names][:shortline] = {}
   
   monopoly[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
-  monopoly[:railroads][:names][:pennsylania_railroad]["mortgage_value"] = "$200"
+  monopoly[:railroads][:names][:pennsylvania_railroad]["mortgage_value"] = "$200"
   monopoly[:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$400"
   monopoly[:railroads][:names][:shortline]["mortgage_value"] = "$800"
   
