3aee67f56fa45ebbbcf8a95d503af3f5647df30c
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 05:18:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,7 +52,32 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
+  monopoly = {}
   
+  monopoly[:railroads] = {}
+  
+  monopoly[:railroads][:pieces] = 4
+  monopoly[:railroads][:names] = {}
+  monopoly[:railroads][:rent_in_dollars] = {}
+  
+  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  
+  
+  monopoly[:railroads][:names][:reading_railroad] = {}
+  monopoly[:railroads][:names][:pennsylania_railroad] = {}
+  monopoly[:railroads][:names][:b_and_o_railroad] = {}
+  monopoly[:railroads][:names][:shortline] = {}
+  
+  monopoly[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+  monopoly[:railroads][:names][:pennsylania_railroad]["mortgage_value"] = "$200"
+  monopoly[:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$400"
+  monopoly[:railroads][:names][:shortline]["mortgage_value"] = "$800"
+  
+  
+  monopoly
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
 end
